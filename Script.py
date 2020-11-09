bb0225d2e7e17514d0921732a4db0d22775f3dfd
neeraj78 <saini.neeraj7788@gmail.com>
2020-11-08 23:24:45 -0500

    Adding Time Frame

--- Script.py
+++ Script.py
@@ -1,10 +1,15 @@
-import cv2
+import cv2 , pandas
+from datetime import datetime
 first_frame = None
 video = cv2.VideoCapture(0)
 
 
+status_list = [None,None]
+times = []
+df = pandas.DataFrame(columns = ["Start" , "End"])
 while True:
     check , frame = video.read()
+    status = 0
     gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray , (21,21) , 0)
 
@@ -16,24 +21,22 @@ while True:
     delta_frame = cv2.absdiff(first_frame , gray)
     thresh_delta = cv2.threshold(delta_frame , 30 , 255 ,cv2.THRESH_BINARY)[1]
     thresh_delta = cv2.dilate(thresh_delta , None , iterations =2)
-    contours, hierarchy = cv2.findContours(thresh_delta, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh_delta, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
     for contour in contours:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 9000:
             continue
+        status = 1
         (x,y,w,h) = cv2.boundingRect(contour)
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    status_list.append(status)
 
 
-
-
-
-
-
-
-
-
+    if status_list[-1]==1 and status_list[-2] == 0 :
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2] == 1 :
+        times.append(datetime.now())
 
 
     cv2.imshow("Frame", frame)
@@ -42,8 +45,16 @@ while True:
 
     key = cv2.waitKey(1)
     if key == ord('q'):
+        if status == 1 :
+            times.append(datetime.now())
         break
+print(status_list)
+print(times)
 
+for i in range(0, len(times), 2):
+    df = df.append({"Start": times[i] , "End":times[i+1]}, ignore_index = True)
+
+df.to_csv("Times.csv")
 
 
 
